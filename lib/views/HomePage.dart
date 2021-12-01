c7bd015d3e1e32deef3b49e37b6aa8da44da8d6a
ramadan <ramadanabdo20295@gmail.com>
2021-12-01 16:33:31 +0200

    finish some updates

--- lib/views/HomePage.dart
+++ lib/views/HomePage.dart
@@ -9,24 +9,24 @@ import 'package:flutter/material.dart';
 import 'MainPage.dart';
 import 'Notification.dart';
 
-
 class HomePage extends StatefulWidget {
   int index;
-  HomePage(int index){
-    this.index=index;
+  HomePage(int index) {
+    this.index = index;
   }
   @override
   _State createState() => _State(this.index);
 }
+
 class _State extends State<HomePage> {
   int _currentIndex;
-  _State(int index){
-    this._currentIndex=index;
+  _State(int index) {
+    this._currentIndex = index;
   }
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
 
-  Home h=new Home();
-  List<Widget>itemsUi=[
+  Home h = new Home();
+  List<Widget> itemsUi = [
     MainPage(),
     Notifications(),
     Cart(),
@@ -37,50 +37,42 @@ class _State extends State<HomePage> {
     // TODO: implement initState
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(h.mainColor),
       key: _scaffoldKey,
-      body:itemsUi[_currentIndex],bottomNavigationBar:
-    BottomNavigationBar(
-        elevation: 15.5,
-        selectedFontSize: 12,
-        unselectedFontSize: 12,
-        backgroundColor: Colors.white,
-        type: BottomNavigationBarType.fixed,
-        selectedItemColor: Color(h.mainColor),
-        unselectedItemColor: Colors.black38
-        ,
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-            // print('${index}index');
-          });
-        },
-        items: [
-          BottomNavigationBarItem(
-              icon:ImageIcon(AssetImage("images/homeIcon.png")),
-              title:  Text(DemoLocalizations.of(context).title['home'],textAlign: TextAlign.center)),
-          BottomNavigationBarItem(
-              icon:  ImageIcon(AssetImage("images/NotificationIcon.png")),
-              title: Text(DemoLocalizations.of(context).title['notification'],textAlign: TextAlign.center,)),
-          BottomNavigationBarItem(
-              icon:  ImageIcon(AssetImage("images/shoppingCard.png")),
-              title: Text(DemoLocalizations.of(context).title['cart'],textAlign: TextAlign.center)),
-          BottomNavigationBarItem(
-              icon:ImageIcon(AssetImage("images/ProfileIcon.png")),
-              title:  Text(DemoLocalizations.of(context).title['profile'],textAlign: TextAlign.center)),
-
-
-
-
-
-        ]),
-
-
-
+      body: itemsUi[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          elevation: 15.5,
+          selectedFontSize: 12,
+          unselectedFontSize: 12,
+          backgroundColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: Color(h.mainColor),
+          unselectedItemColor: Colors.black38,
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+              // print('${index}index');
+            });
+          },
+          items: [
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("images/homeIcon.png")),
+                label: DemoLocalizations.of(context).title['home']),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("images/NotificationIcon.png")),
+                label: DemoLocalizations.of(context).title['notification']),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("images/shoppingCard.png")),
+                label: DemoLocalizations.of(context).title['cart']),
+            BottomNavigationBarItem(
+                icon: ImageIcon(AssetImage("images/ProfileIcon.png")),
+                label: DemoLocalizations.of(context).title['profile']),
+          ]),
     );
   }
 }
